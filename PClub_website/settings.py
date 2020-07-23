e396c3c9d82e36076409151ce5987a03b8e60c5a
dhatrikapuriya <46780667+dhatrikapuriya@users.noreply.github.com>
2020-07-23 12:42:05 +0530

    Update settings.py

--- PClub_website/settings.py
+++ PClub_website/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+from secrets import *
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -19,11 +19,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ps(!rzkrbxu45s%xghami$7f6-#4puo0*%%!^jx_*xfqpk*$#c'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
